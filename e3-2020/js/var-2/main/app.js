91768f34aec3b7c1d031a2647576a904927f02b3
Anastasia <anastasia.pintilie98@gmail.com>
2020-02-03 22:03:55 +0000

    js rezolvat complet

---
+++ e3-2020/js/var-2/main/app.js
@@ -0,0 +1,37 @@
+class Duck{
+    constructor(name){
+        if(typeof(name)!="string" && typeof(name)!="String") throw new Error("name must be string or String")
+        this.name = name
+    }
+    move(){
+        return `${this.name} is moving`
+    }
+    swim()
+    {
+         return `${this.name} is swimming`
+    }
+}
+
+class RubberDuck extends Duck{
+    constructor(name)
+    {
+     super(name);   
+    }
+        float(){
+        return `${this.name} floats`
+    }
+    swim()
+    {
+         return `${this.name} can't swim, only float`
+    }
+    
+}
+
+
+
+const app = {
+    Duck,
+    RubberDuck
+}
+
+module.exports = app
